74a1cba75f7b9ef88dd3d41aaa550df0a40cdef8
Paul <plai@champ-sys.com>
2020-06-29 10:35:00 +0800

    Updates

---
+++ precache-manifest.952c9f49fdec60b21076b5bda7ccc8ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc372584444c841b8b138e225b050c1d",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "c0b4e06eec2e9aae907d",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d69056f0358d2712cd27",
+    "url": "/react-gh-pages/static/js/2.d498de8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.d498de8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b4e06eec2e9aae907d",
+    "url": "/react-gh-pages/static/js/main.d5774e92.chunk.js"
+  },
+  {
+    "revision": "08448035568596b86315",
+    "url": "/react-gh-pages/static/js/runtime-main.252166f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
